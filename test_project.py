97bd6919a5bafe4fc15499be74227cf71c8a7aa2
metaphorshear <metaphorshear@users.noreply.github.com>
2025-10-26 20:30:36 -0500

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -2,6 +2,6 @@ import pitch_bend as pb
 
 class TestPitchBend:
   def test_vibrato(self):
-    input = [1, 1, 1, 1, 1, 1]
+    input = np.array([1, 1, 1, 1, 1, 1, 1])
     t = pb.vibrato(input, 100, 0.25)
     assert t != input
